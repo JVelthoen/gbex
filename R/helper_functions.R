61f35dbfbb0d6f2f95f9dc906dee97c1a02848ad
Jasper Velthoen <j.j.velthoen@tudelft.nl>
2019-10-29 17:28:45 +0100

    Addition of power divergence
    sigma does converge when setting alpha = 0.5. The gamma parameter does not converge leads to severe overestimation of gamma.

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -3,18 +3,20 @@
 #' @param y A set of observations
 #' @param newdata A data frame with covariates for which to predict the sigma and gamma parameter
 #' @return A data.frame object with the estimated sigma and gamma parameters
+#' @export
 first_guess <- function(y){
   GP_fit=POT::fitgpd(y,threshold=0)
   theta_init=GP_fit$fitted.values
-  theta_init=matrix(rep(theta_init,n),ncol=2,byrow=T)
+  theta_init=matrix(rep(theta_init,length(y)),ncol=2,byrow=T)
   colnames(theta_init)=c('s','g')
   return(theta_init)
 }
 
-#' For a gradient tree fill the leafnodes with the gradient update gamma
+#' For a gradient tree fill the leafnodes with the gradient update gamma (CURRENTLY NOT USED)
 #'
 #' @param tree a gradient tree
 #' @return A data.frame object with for each leaf the gradient update
+#' @export
 get_leaf_values <- function(tree){
   gamma <- predict(tree)
   leaf <- treeClust::rpart.predict.leaves(tree)
@@ -25,28 +27,30 @@ get_leaf_values <- function(tree){
 #' Fill the leafnodes of the sigma tree using linesearch
 #'
 #' @param tree a gradient tree
-#' @param DF_boost the boosting data.frame
+#' @param DF_boost the boosting dataframe used for obtaining the tree
 #' @return A data.frame object with for each leaf the gradient update
-fill_leafs_s <- function(tree,DF_boost){
-  update_df <- data.frame(leaf = treeClust::rpart.predict.leaves(tree,newdata=DF_boost)) %>%
-      dplyr::mutate(d1 = DF_boost$r_s, d2 = DF_boost$r2_s) %>%
-      dplyr::group_by(leaf) %>% dplyr::summarise(d1=mean(d1),d2=mean(d2)) %>%
-      dplyr::mutate(gamma = d1/d2) %>%
-      dplyr::select(gamma,leaf)
+#' @export
+fill_leafs_s <- function(tree,DF_tree){
+  update_df <- data.frame(leaf = tree$where, r = DF_tree$r_s, r2 = DF_tree$r2_s) %>%
+      dplyr::group_by(leaf) %>% dplyr::summarise(r=sum(r),r2=sum(r2)) %>%
+      dplyr::mutate(gamma = r/r2) %>%
+      dplyr::select(gamma,leaf) %>%
+      dplyr::mutate(gamma = ifelse(abs(gamma)<1,gamma,gamma/abs(gamma)))
   return(update_df)
 }
 
 #' Fill the leafnodes of the gamma tree using linesearch
 #'
 #' @param tree a gradient tree
-#' @param DF_boost the boosting data.frame
+#' @param DF_tree the boosting dataframe used for obtaining the tree
 #' @return A data.frame object with for each leaf the gradient update
-fill_leafs_g <- function(tree,DF_boost){
-  update_df <- data.frame(leaf = treeClust::rpart.predict.leaves(tree,newdata=DF_boost)) %>%
-    dplyr::mutate(d1 = DF_boost$r_g, d2 = DF_boost$r2_g) %>%
-    dplyr::group_by(leaf) %>% dplyr::summarise(d1=mean(d1),d2=mean(d2)) %>%
-    dplyr::mutate(gamma = d1/d2) %>%
-    dplyr::select(gamma,leaf)
+#' @export
+fill_leafs_g <- function(tree,DF_tree){
+  update_df <- data.frame(leaf = tree$where, r = DF_tree$r_g, r2 = DF_tree$r2_g) %>%
+    dplyr::group_by(leaf) %>% dplyr::summarise(r=mean(r),r2=mean(r2)) %>%
+    dplyr::mutate(gamma = r/r2) %>%
+    dplyr::select(gamma,leaf) %>%
+    dplyr::mutate(gamma = ifelse(abs(gamma)<1,gamma,gamma/abs(gamma)))
   return(update_df)
 }
 
@@ -56,8 +60,8 @@ fill_leafs_g <- function(tree,DF_boost){
 #' @param TREE_s a list of gradient tree for sigma together with a df with gradient step per leafnode
 #' @param DF_boost the current boosting data frame
 #' @param lambda a vector of length two with learning rate for sigma and gamma
-#'
 #' @return A data.frame object with for each leaf the gradient update
+#' @export
 update_parameter <- function(TREE_s,TREE_g,DF_boost,lambda){
   gamma_s <- data.frame(leaf = treeClust::rpart.predict.leaves(TREE_s$tree,newdata=DF_boost)) %>%
     dplyr::left_join(TREE_s$values,by="leaf") %>% .$gamma
@@ -75,16 +79,27 @@ update_parameter <- function(TREE_s,TREE_g,DF_boost,lambda){
 #' @param theta the new parameter updates for sigma and gamma
 #'
 #' @return A data.frame object with the data the parameter estimates the negative likelihood and the first and second derivatives of the negative likelihood towards sigma and gamma
-get_DF_boost <- function(DF,theta){
+#' @export
+get_DF_boost <- function(DF,theta,alpha=0){
   data <- DF[ colnames(DF)[!(colnames(DF) %in% c("s_hat","g_hat","dev","r_s","r_g","r2_s","r2_g"))]]
   s_hat=theta[,'s']
   g_hat=theta[,'g']
-  dev=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev)
-  r_s=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff_s)
-  r_g=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff_g)
-  r2_s=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff2_s)
-  r2_g=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff2_g)
-
+  if(alpha==0){
+    dev=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev)
+    r_s=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff_s)
+    r_g=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff_g)
+    r2_s=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff2_s)
+    r2_g=apply(cbind(s_hat,g_hat,DF$y),1,GP_dev_diff2_g)
+    DF_dev = data.frame(s_hat=s_hat, g_hat=g_hat, dev=dev, r_s=r_s, r_g=r_g, r2_s=r2_s, r2_g=r2_g)
+  } else{
+    A <- apply(cbind(s_hat,g_hat,DF$y),1,A_func)
+    B <- apply(cbind(s_hat,g_hat,DF$y),1,B_func)
+    dev=apply(cbind(s_hat,g_hat,DF$y,A,B),1,function(x) PD_dev(x[1:3],x[4],x[5],alpha))
+    r_s=apply(cbind(s_hat,g_hat,DF$y,A,B),1,function(x) PD_dev_diff_s(x[1:3],x[4],x[5],alpha))
+    r_g=apply(cbind(s_hat,g_hat,DF$y,A,B),1,function(x) PD_dev_diff_g(x[1:3],x[4],x[5],alpha))
+    r2_s=apply(cbind(s_hat,g_hat,DF$y,A,B),1,function(x) PD_dev_diff2_s(x[1:3],x[4],x[5],alpha))
+    r2_g=apply(cbind(s_hat,g_hat,DF$y,A,B),1,function(x) PD_dev_diff2_g(x[1:3],x[4],x[5],alpha))
+  }
   DF_boost=data.frame(data, s_hat=s_hat, g_hat=g_hat, dev=dev, r_s=r_s, r_g=r_g, r2_s=r2_s, r2_g=r2_g)
   return(DF_boost)
 }
---
+++ documentation/example.R
@@ -0,0 +1,49 @@
+## example script
+
+set.seed(25051979)
+
+### Data generating process ###
+n <- 1000
+d <- 2
+data <- get_data(n,d)
+s <- data$s # the sigma parameter
+g <- data$g # the gamma parameter
+y <- data$y # the response vector
+X <- data.frame(X=data$X) # the covariates
+
+
+B <-  250 # The number of gradient boosting steps
+lambda=c(0.01,0.0025) # The learning rate for the sigma and gamma parameter
+depth=c(2,2) # The maximum depth for a single sigma and gamma tree
+min_leaf_size=c(30,30) # The minimum number of observations contained in the leafnodes
+sf=0.75 # The subsample fraction used for estimating each tree (in a single step only one subsample is drawn and used for both the sigma and gamma tree)
+
+fit <- gbex(y,X,B=B,lambda=lambda,depth=depth,min_leaf_size=min_leaf_size,sf=sf)
+
+x_bar=apply(X^2, 1,mean)
+par(mfrow=c(1,3))
+plot(x_bar,s,pch='.', col='red',ylim=c(min(c(s,fit$s_hat)),max(c(s,fit$s_hat))))
+points(x_bar,fit$s_hat,pch='.',col='blue')
+plot(x_bar,g,pch='.', col='red',ylim=c(min(c(g,fit$g_hat)),max(c(g,fit$g_hat))))
+points(x_bar,fit$g_hat,pch='.',col='blue')
+plot(fit$dev,xlab="iteration",ylab="deviance")
+
+y_test <- s*(runif(n)^(-g)-1)/g
+theta_hat <- predict(fit,newdata=X)
+s_hat_test <- theta_hat$s
+g_hat_test <- theta_hat$g
+
+par(mfrow=c(1,2))
+plot(x_bar,s,pch='.', col='red',ylim=c(min(c(s,s_hat_test)),max(c(s,s_hat_test))))
+points(x_bar,s_hat_test,pch='.',col='blue')
+plot(x_bar,g,pch='.', col='red',ylim=c(min(c(g,g_hat_test)),max(c(g,g_hat_test))))
+points(x_bar,g_hat_test,pch='.',col='blue')
+
+dev <- dev_per_step(fit)
+dev_test <- dev_per_step(fit,X=X,y=y_test)
+minimum_iteration <- (0:B)[which(dev_test==min(dev_test))]
+par(mfrow=c(1,1))
+plot(0:B,dev,type="l",xlab="iteration 0,..,B",col="blue",ylim=c(min(dev,dev_test),max(dev,dev_test)))
+lines(0:B,dev_test,col="red")
+abline(v=minimum_iteration,lty=2,lwd=2)
+
